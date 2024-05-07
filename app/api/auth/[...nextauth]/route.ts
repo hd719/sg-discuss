2679740f1452aa7c8ead4acfe2099a4b6bcd699d
Hamel Desai <hameldesai93@gmail.com>
2024-05-07 18:19:54 -0400

    section-7-video-60

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,9 @@
+// You only need these routes if you want an external API to interact with the next app
+// In this case Github servers will reach out to our applications
+
+// export async function GET() {}
+
+// export async function POST() {}
+
+export { GET, POST } from "@/auth";
+
