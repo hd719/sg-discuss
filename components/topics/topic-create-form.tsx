a2bac930390fa54020c1c0641d7b21a27054b780
Hamel Desai <hameldesai93@gmail.com>
2024-05-08 15:35:35 -0400

    section-7-video-75

--- components/topics/topic-create-form.tsx
+++ components/topics/topic-create-form.tsx
@@ -1,3 +1,37 @@
+import {
+  Input,
+  Button,
+  Textarea,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@nextui-org/react";
+import * as actions from "@/actions";
+
 export default function TopicCreateForm() {
-  return <div>Topic Create Form</div>;
+  return (
+    <Popover placement="left">
+      <PopoverTrigger>
+        <Button color="primary">Create a Topic</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <form action={actions.createTopic}>
+          <div className="flex flex-col gap-4 p-4 w-80">
+            <h3 className="text-lg">Create a topic</h3>
+            <Input
+              label="Name"
+              labelPlacement="outside"
+              placeholder="Name of topic"
+            />
+            <Textarea
+              label="Description"
+              labelPlacement="outside"
+              placeholder="Description of topic"
+            />
+            <Button type="submit">Submit</Button>
+          </div>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
 }
