7a56c1a731167b604ceb681a0f981078dbee2e98
Hamel Desai <hameldesai93@gmail.com>
2024-05-08 15:27:15 -0400

    section-7-video-74

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@
 // import { auth } from "@/auth";
 // import Profile from "@/components/profile";
 
+import TopicCreateForm from "@/components/topics/topic-create-form";
+
 export default async function Home() {
   // const session = await auth();
 
@@ -23,7 +25,14 @@ export default async function Home() {
       )}
 
       <Profile /> */}
-      HomePage
+      <div className="grid grid-cols-4 gap-4 p-4">
+        <div className="col-span-3">
+          <h1 className="text-xl m-2">Top Posts</h1>
+        </div>
+        <div>
+          <TopicCreateForm />
+        </div>
+      </div>
     </div>
   );
 }
