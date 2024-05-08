b18d89521b4ae3e1dc42da9b7b37a8de3dbb1fff
Hamel Desai <hameldesai93@gmail.com>
2024-05-08 16:21:47 -0400

    section-7-video-77

--- actions/create-topic.ts
+++ actions/create-topic.ts
@@ -1,9 +1,24 @@
 "use server";
 
-export async function createTopic(formData: FormData) {
-  const name = formData.get("name") as string;
-  const description = formData.get("description") as string;
+import { z } from "zod";
 
-  // Validation
-  // Revalidate the homepage
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/[a-z-]/, {
+      message: "Name must be lowercase and contain only letters and no spaces",
+    }),
+  description: z.string().min(10),
+});
+
+export async function createTopic(formData: FormData) {
+  const result = createTopicSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+
+  if (!result.success) {
+    console.log(result.error.flatten());
+  }
 }
