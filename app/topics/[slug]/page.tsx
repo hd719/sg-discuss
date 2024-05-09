c235adbab153c9c75d32fa60e44318804bb4435d
Hamel Desai <hamel.desai@axio.com>
2024-05-09 14:21:20 -0400

    section-8-video-86

--- app/topics/[slug]/page.tsx
+++ app/topics/[slug]/page.tsx
@@ -1,7 +1,17 @@
-export default function TopicShowPage() {
+interface TopicShowPageProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default function TopicShowPage({ params }: TopicShowPageProps) {
+  const { slug } = params;
+
   return (
-    <div>
-      <h1>TopicShowPage</h1>
+    <div className="grid grid-cols-4 gap-4 p4">
+      <div className="col-span-3">
+        <h1 className="text-2xl font-bold-mb-2 ">{slug}</h1>
+      </div>
     </div>
   );
 }
