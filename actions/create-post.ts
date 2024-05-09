c1b74f2b7f081f8969dbacbfbc22a55f897dc428
Hamel Desai <hamel.desai@axio.com>
2024-05-09 14:36:52 -0400

    section-8-video-88

--- actions/create-post.ts
+++ actions/create-post.ts
@@ -1,6 +1,42 @@
 "use server";
+import type { Post } from "@prisma/client";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+import { auth } from "@/auth";
+import paths from "@/path";
+import { prisma } from "@/db";
+import { revalidatePath } from "next/cache";
 
-export async function createPost() {
-  // Revalidate the topic show page
-  return { post: "New Post" };
+const createPostSchema = z.object({
+  title: z.string().min(3),
+  content: z.string().min(10),
+});
+
+// This is what we are returning from the createPost function
+interface CreatePostFormState {
+  errors: {
+    title?: string[];
+    content?: string[];
+    _form?: string[];
+  };
+}
+
+export async function createPost(
+  formState: CreatePostFormState,
+  formData: FormData
+): Promise<CreatePostFormState> {
+  const result = createPostSchema.safeParse({
+    title: formData.get("title"),
+    content: formData.get("content"),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  return {
+    errors: {},
+  };
 }
