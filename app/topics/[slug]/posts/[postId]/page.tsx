c3dcc90b95dddf242f2f4cd257eacdba1c724c6b
Hamel Desai <hamel.desai@axio.com>
2024-05-09 17:34:59 -0400

    section-8-video-99

--- app/topics/[slug]/posts/[postId]/page.tsx
+++ app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,7 +1,30 @@
-export default function PostShowPage() {
+import Link from "next/link";
+import PostShow from "@/components/posts/post-show";
+import CommentList from "@/components/comments/comment-list";
+import CommentCreateForm from "@/components/comments/comment-create-form";
+import paths from "@/path";
+
+interface PostShowPageProps {
+  params: {
+    slug: string;
+    postId: string;
+  };
+}
+
+export default async function PostShowPage({ params }: PostShowPageProps) {
+  const { slug, postId } = params;
+
   return (
-    <div>
-      <h1>PostShowPage</h1>
+    <div className="space-y-3">
+      <Link
+        className="underline decoration-solid"
+        href={paths.topicShowPath(slug)}
+      >
+        {"< "}Back to {slug}
+      </Link>
+      <PostShow postId={postId} />
+      {/* <CommentCreateForm postId={postId} /> */}
+      {/* <CommentList /> */}
     </div>
   );
 }
