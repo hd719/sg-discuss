43635d54d3124a46a197d74fec563fbad8b85625
Hamel Desai <hamel.desai@axio.com>
2024-05-09 17:21:52 -0400

    section-8-video-99

--- db/queries/post.ts
+++ db/queries/post.ts
@@ -2,13 +2,17 @@ import type { Post } from "@prisma/client";
 import { prisma } from "@/db";
 import { select } from "@nextui-org/react";
 
-export type PostWithData = Post & {
-  topic: { slug: string };
-  user: { name: string | null };
-  _count: { comments: number };
-};
+// export type PostWithData = Post & {
+//   topic: { slug: string };
+//   user: { name: string | null };
+//   _count: { comments: number };
+// };
 
-export function fetchPostByTopicSlug(slug: string): Promise<PostWithData[]> {
+export type PostWithData = Awaited<
+  ReturnType<typeof fetchPostByTopicSlug>
+>[number];
+
+export function fetchPostByTopicSlug(slug: string) {
   // Find a post by a given slug, and include the topic, user, and comment count
   const posts = prisma.post.findMany({
     where: { topic: { slug } },
