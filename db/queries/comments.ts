87bec2916c351ae27e6a5e2673c4fe3f9cf54bd9
Hamel Desai <hameldesai93@gmail.com>
2024-07-30 10:53:18 -0400

    section-9-video-107

--- db/queries/comments.ts
+++ db/queries/comments.ts
@@ -1,19 +1,21 @@
 import type { Comment } from "@prisma/client";
+import { cache } from "react";
 import { prisma } from "@/db";
 
 export type CommentWithAuthor = Comment & {
   user: { name: string | null; image: string | null };
 };
 
-export function fetchCommentByPostId(
-  postId: string
-): Promise<CommentWithAuthor[]> {
-  return prisma.comment.findMany({
-    where: { postId },
-    include: {
-      user: {
-        select: { name: true, image: true },
+export const fetchCommentByPostId = cache(
+  (postId: string): Promise<CommentWithAuthor[]> => {
+    console.log("Making a request to the database");
+    return prisma.comment.findMany({
+      where: { postId },
+      include: {
+        user: {
+          select: { name: true, image: true },
+        },
       },
-    },
-  });
-}
+    });
+  }
+);
